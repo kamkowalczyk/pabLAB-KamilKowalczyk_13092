921c38cdb542d61a87fa671bc03c6ccddc07c534
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-03-09 18:20:18 +0100

    done2

--- index.js
+++ index.js
@@ -3,39 +3,50 @@ var app = express();
 app.get('/', function (req, res) {  
     res.send('Hello World')  
   }); 
-app.get('/dodaj', function (req, res) {
-    let a =Number(req.query.num1);
-    let b =Number(req.query.num2);
-    a=2;
-    b=3;
+app.get('/dodaj/:num1/:num2', function (req, res) {
+    const a =Number(req.params.num1);
+    const b =Number(req.params.num2);
+    //a=2;
+    //b=3;
     let result = a+b;
     res.send(`Wynik dodawania ${result}`);
     
 });
-app.get('/usun', function (req, res) {
-    let a =Number(req.query.num1);
-    let b =Number(req.query.num2);
-    a=4;
-    b=2;
+//http://localhost:3000/dodaj?num1=5&num2=10operation=dodaj
+app.get('/odejmowanie/:num1/:num2', function (req, res) {
+    const a =Number(req.params.num1);
+    const b =Number(req.params.num2);
+   // a=4;
+    //b=2;
     let result = a-b;
     res.send(`Wynik odejmowania ${result}`);
     
 });
-app.get('/mnozenie', function (req, res) {
-    let a =Number(req.query.num1);
-    let b =Number(req.query.num2);
+// app.get('/mnozenie', function (req, res) {
+//     const a =Number(req.query.num1);
+//     const b =Number(req.query.num2);
 
-    a=4;
-    b=5;
+//    // a=4;
+//    // b=5;
+//     let result = a*b;
+//     res.send(`Wynik mnożenia ${result}`);
+    
+// });
+app.get('/mnozenie/:num1/:num2', function (req, res) {
+    const a =Number(req.params.num1);
+    const b =Number(req.params.num2);
+
+   // a=4;
+   // b=5;
     let result = a*b;
     res.send(`Wynik mnożenia ${result}`);
     
 });
-app.get('/dzielenie', function (req, res) {
-    let a =Number(req.query.num1);
-    let b =Number(req.query.num2);
-    a=6;
-    b=2;
+app.get('/dzielenie/:num1/:num2', function (req, res) {
+    const a =Number(req.params.num1);
+    const b =Number(req.params.num2);
+   // a=6;
+   // b=2;
     let result = a/b;
     res.send(`Wynik dzielenia ${result}`);
     
